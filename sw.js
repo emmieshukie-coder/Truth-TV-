226cd1acbd1191ab88cdd4fcdb105f1741493623
emmieshukie-coder <emmieshukie@gmail.com>
2026-06-16 05:55:55 +0300

    Add service worker with caching functionality
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+const CACHE_NAME = 'truth-tv-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
